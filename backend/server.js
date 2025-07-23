98292e4c9c4a511a2dfce9a98590bbab1b0ca782
ogubuike245 <ogubuike245@gmail.com>
2025-07-23 03:24:54 +0100

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -23,30 +23,25 @@ const limiter = rateLimit({
 // Middleware
 app.use(helmet());
 app.use(limiter);
-app.use(cors({ origin: "*", credentials: true }));
+const allowedOrigins = [process.env.FRONTEND_URL];
 
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       // Allow requests with no origin (like mobile apps or curl requests)
-//       if (!origin) return callback(null, true);
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      console.log("Incoming Origin:", origin);
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(null, false); // No error thrown
+      }
+    },
+    credentials: true,
+  })
+);
 
-//       // Check if the origin is allowed
-//       const allowedOrigins = [
-//         process.env.FRONTEND_URL,
+// Allow preflight for all routes
+app.options("*", cors());
 
-//         // Add any other frontend URLs that need access
-//       ];
-
-//       if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true,
-//   })
-// );
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
